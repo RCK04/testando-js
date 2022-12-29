c098121b74d2064b80be842e2eecaa445e397e68
Erick Roberto <erickkan2004@gmail.com>
2022-12-28 22:08:52 -0300

    main

---
+++ script.js
@@ -0,0 +1,102 @@
+/* function validar(CorrectUser, CorrectPassWord){
+
+    let usuario = 'pedro'
+    let senha = '123'
+
+    if(usuario != CorrectUser || senha != CorrectPassWord){
+        console.log('Acesso NEGADO')
+    } else{
+        console.log('Acesso Concedido')
+    }
+}
+
+let UserName = 'pedro'
+let UserPassWord = '123'
+
+let validacao = validar(UserName, UserPassWord)
+
+
+
+function validar(usuario, senha){
+    if(usuario === 'pedro' && senha == '123'){
+        return true
+    } else{
+        return false
+    }
+}
+
+let usuario = 'pedro'
+let senha = '123'
+let validacao = validar(usuario, senha)
+
+if(validacao){
+    console.log('Acesso Concedido')
+} else{
+    console.log('Acesso Negado')
+}
+
+
+
+function somar(x, y){
+    return x + y
+}
+
+const sobrenome = (sob) => {
+    let nomeCompleto = 'Bonieky ' + sob
+
+    return nomeCompleto
+}
+
+console.log(sobrenome('Pereira'))
+
+
+
+
+
+function addSquares(a, b){
+    const square = (x) => {
+        return x * x;
+    }
+
+    return square(a) + square(b);
+}
+
+console.log(addSquares(10,10))
+
+
+
+let ingredientes = 
+[
+    'agua',
+    'farinha',
+    'ovo',
+    'corante',
+    'sal'
+];
+// Adicionar no Array usar PUSH
+//ingredientes.push('cebola')
+
+//Remover último item do Array POP
+//ingredientes.pop()
+
+//Remover primeiro item do Array Shift
+//ingredientes.shift()
+
+console.log(`Total de Ingredientes: ${ingredientes.length}`);
+
+
+
+let carros = ['BMW', 'Ferrari', 'Mercedes'];
+let x = 1
+console.log('1. ' + carros[x])
+
+carros[1] = 'Audi'
+
+console.log(carros)
+
+carros.push('Volvo')
+console.log(carros)
+console.log(carros.length)
+
+*/
+
