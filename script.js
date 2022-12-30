62dd7a79d5e21454c31aca4314d153dc9b5218f4
Erick Roberto <erickkan2004@gmail.com>
2022-12-29 21:28:55 -0300

    main

--- script.js
+++ script.js
@@ -98,5 +98,86 @@ carros.push('Volvo')
 console.log(carros)
 console.log(carros.length)
 
+
+// Objetos
+
+let nome = 'Bonieky';
+let nomes = ['bonieky', 'pedro']
+
+// Criar objeto
+let personagem = {
+    nome: 'Carlos',
+    idade: 21,
+    olhos: ['preto', 'azul'],
+    caracteristicas: {
+
+        classe: 'Assassino',
+        poder: 'Chamas'
+    },
+    pais: 'Brasil'
+}
+
+personagem.nome = 'Erick'
+personagem.idade = 18
+personagem.caracteristicas.classe = 'Arqueiro Assassino [Lendário]'
+personagem.caracteristicas.poder = 'Chamas Negra'
+
+personagem.idade += 5
+personagem.olhos.push('verde')
+
+console.log(`Seu nome é ${personagem.nome} e você têm ${personagem.idade} anos, sua classe é ${personagem.caracteristicas.classe} usuário de ${personagem.caracteristicas.poder}`)
+console.log(personagem.olhos[2])
+
+
+let pessoa = {
+    nome: 'Erick',
+    idade: 18,
+    carros: [
+        {modelo: 'Fiat', cor: 'Preto'},
+        {modelo: 'Ferrari', cor: 'vermelho'}
+    ]
+}
+
+console.log(pessoa.carros[1].modelo)
+
 */
+
+/*
+
+let pessoa = {
+    nome: 'Neymar',
+    sobrenome: 'Junior',
+    idade: 18,
+    /*nomeCompleto: function(){
+        return this.nome + ' ' + this.sobrenome
+    }
+
+
+    nomeCompleto: () => {
+        return pessoa.nome + ' ' + pessoa.sobrenome
+    }
+
+}
+
+console.log(pessoa.nomeCompleto())
+
+*/
+
+let frutas = [
+    {cor: 'verde', qt: 5},
+    {cor: 'azul', qt: 1},
+    {cor:  'vermelho', qt: 10},
+]
+
+//for(let contador = 0; contador < frutas.length; contador++){
+//    console.log(frutas[contador])
+//}
+
+//for(let i in frutas){
+  //  console.log(frutas[i])
+//}
+
+for(let fruta of frutas){
+    console.log(`Cor da Fruta: ${fruta.cor} - Quantidade: ${fruta.qt}`)
+}
 
